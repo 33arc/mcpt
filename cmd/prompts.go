f6828e924d253909240280d8a61d92d6b3ee4897
33arc <quickselect@proton.me>
2025-08-19 09:11:49 +0300

    feat: add features 'prompts' and 'resources'

---
+++ cmd/prompts.go
@@ -0,0 +1,38 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"github.com/33arc/mcpt/mcp"
+	"github.com/spf13/cobra"
+)
+
+var promptsCmd = &cobra.Command{
+	Use:   "prompts",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		client := mcp.NewClient(host, false)
+		client.ListFeature("prompts")
+	},
+}
+
+func init() {
+	listCmd.AddCommand(promptsCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// serverCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// serverCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
